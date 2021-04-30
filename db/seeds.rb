e3f227600574150687eb5955c59edcacf423f49a
taijusugahara <96ayutuki@gmail.com>
2021-04-30 09:25:28 +0900

    corporationのemail,password変更して秘密鍵化

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Corporation.find_or_create_by(id: 1) do |cor|
-  cor.email = 'corporation@1.com'
-  cor.password = 'corporation'
+  cor.email = ENV['CORPORATION_EMAIL']
+  cor.password = ENV['CORPORATION_PASSWORD']
 end
 
